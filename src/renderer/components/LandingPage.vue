5b32f367d70538f87f678ebea3b34571ae596360
Tom Elliott <tomelliottnz@gmail.com>
2018-12-16 15:58:51 +1300

    Change title to trigger build

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -4,7 +4,7 @@
     <main>
       <div class="left-side">
         <span class="title">
-          Welcome to your new project!
+          iNZight
         </span>
         <system-information></system-information>
       </div>
